eaa89099aa86f66e77419b1a6e059bf478528903
MeylinLeonario <meylin.leonario@alumnos.ucn.cl>
2026-08-17 21:10:23 -0400

    fix Dockerfile casing

--- backend.api/obj/Debug/net10.0/backend.api.AssemblyInfo.cs
+++ backend.api/obj/Debug/net10.0/backend.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ec9c8924942f91700b71da6418cb725e6b78c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a43e96c9a4b7af44ff14adeb99e144eafa529565")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
